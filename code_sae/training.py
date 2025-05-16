482975b943e31c5eb4768ff45d82831df9fe9384
Rui Melo <rufimelo99@gmail.com>
2025-05-16 01:21:07 +0100

    Update training.py

--- code_sae/training.py
+++ code_sae/training.py
@@ -38,6 +38,7 @@ def train(config):
         **config,
         lr_decay_steps=lr_decay_steps,  # this will help us avoid overfitting.
         l1_warm_up_steps=l1_warm_up_steps,  # this can help avoid too many dead features initially.
+        total_training_tokens=total_training_tokens,
         device=DEVICE,
         seed=SEED,
     )
