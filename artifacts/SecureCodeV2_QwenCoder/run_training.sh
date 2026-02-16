fcb3ccafdace5b175da25d972f229290c6b4d323
Rui Melo <rufimelo99@gmail.com>
2026-02-16 16:51:32 -0500

    Create run_training.sh

---
+++ artifacts/SecureCodeV2_QwenCoder/run_training.sh
@@ -0,0 +1,3 @@
+python code_sae/training.py --config artifacts/SecureCodeV2_QwenCoder/_training_config_layer0_topk_16384_lr_1e-4.json 
+python code_sae/training.py --config artifacts/SecureCodeV2_QwenCoder/_training_config_layer14_topk_16384_lr_1e-4.json
+python code_sae/training.py --config artifacts/SecureCodeV2_QwenCoder/_training_config_layer27_topk_16384_lr_1e-4.json
